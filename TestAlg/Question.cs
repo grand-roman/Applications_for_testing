b72aa53a91f8180e13ad3f1deb0a587869000200
Roman <31464179+grand-roman@users.noreply.github.com>
2019-02-19 21:14:54 +0700

    Created_project
    I created the project.
    Created xml files and registered their connection to objects.

---
+++ TestAlg/Question.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace TestAlg
+{
+    class Question
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public List<string> AnswerOptions { get; set; }
+
+        public Question()
+        {
+            AnswerOptions = new List<string>();
+        }
+
+        public static Question LoadFromXml(XElement Ques)
+        {
+            var res = new Question();
+            res.Id = (int)Ques.Element("id");
+            res.Text = (string)Ques.Element("text");
+            res.AnswerOptions.Add((string)Ques.Element("answer"));
+            foreach(string dis in Ques.Elements("distractor"))
+                res.AnswerOptions.Add(dis);
+
+
+            return res;
+        }
+    }
+
+
+}
---
+++ TestAlg/QuestionBlank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TestAlg
+{
+    class QuestionBlank
+    {
+        public List<Question> Questions { get; set; }
+
+        public QuestionBlank()
+        {
+            Questions = new List<Question>();
+        }
+
+        public static QuestionBlank LoadFromXml(XElement Ques)
+        {
+            var res = new QuestionBlank();
+            foreach (var ques in Ques.Elements())
+
+                res.Questions.Add(Question.LoadFromXml(ques));
+
+            return res;
+        }
+    }
+}
