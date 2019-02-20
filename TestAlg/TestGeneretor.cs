46444ac9cd48baacf26bca1f90c0ca0f96643a89
Roman <31464179+grand-roman@users.noreply.github.com>
2019-02-20 16:21:03 +0700

    VM for test with question generation
    Added test generator class and added VM class

---
+++ TestAlg/TestGeneretor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAlg
+{
+    class TestGeneretor
+    {
+        public static List<Question> Generate(QuestionBlank bank)
+        {
+
+            return bank.Questions;
+        }
+    }
+}
